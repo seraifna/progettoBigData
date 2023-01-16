509cb24c3dd0f721211474ea798dff9a1b0c659a
Serafina Narda <serafina.narda@yahoo.it>
2023-01-16 18:50:35 +0100

    project setup

---
+++ src/main/java/com/example/progettobigdata/ProgettoBigdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettobigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBigdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoBigdataApplication.class, args);
+    }
+
+}
